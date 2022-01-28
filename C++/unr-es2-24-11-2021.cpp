80983b2f73d303d11012e17b69714dab33914264
Alice <maintrax80craft@gmail.com>
2022-01-28 18:41:33 +0100

    Ok

--- C++/unr-es2-24-11-2021.cpp
+++ C++/unr-es2-24-11-2021.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int eta;
     cout << "Inserire l'età :";
     cin >> eta;
-    while(eta <= 0 || eta > 127) {   //Keyboard control for user ages (AntiFake-UserAges)
+    while(eta <= 0 || eta > 127)  //Keyboard control for user ages (AntiFake-UserAges)
+    {   
         cout << "Reinserire l'età :";
         cin >> eta;
     }
+
     if(eta >= 18)
-     cout << "Sei maggiorenne" << endl;
+    {
+      cout << "Sei maggiorenne" << endl;  
+    }
     else
-     cout << "Sei minorennne" << endl;
+    {
+      cout << "Sei minorennne" << endl;  
+    }
     return(0);
 }
